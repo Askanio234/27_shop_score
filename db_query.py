691d3b6b5247cd9b162d9f6bd4a90cd41662accf
Artem Chernyaev <askanio234@gmail.com>
2017-06-20 18:46:49 +0300

    Fixed crush when there is no unconfirmed orders

--- db_query.py
+++ db_query.py
@@ -7,8 +7,11 @@ def get_order_waiting_time():
                                                     orders.confirmed.is_(None)
                                                     ).order_by(
                                                     orders.created).first()
-    time_delta = datetime.datetime.now() - first_unconfirmed_order.created
-    return round(time_delta.seconds/60, 1)
+    if first_unconfirmed_order:
+        time_delta = datetime.datetime.now() - first_unconfirmed_order.created
+        return round(time_delta.seconds/60, 1)
+    else:
+        return 0.0
 
 
 def get_unprocessed_orders():
